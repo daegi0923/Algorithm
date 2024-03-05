f65b38ba3b31bea5617fb4988a2901b373adc295
daegi0923 <156268579+daegi0923@users.noreply.github.com>
2024-03-05 15:33:07 +0900

    [Silver V] Title: 분수찾기, Time: 128 ms, Memory: 33480 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,17 @@
+from re import I
+
+
+n = int(input())
+loc = 0
+num = 0
+for i in range(1, 10000000):
+    num += i
+    loc += 1
+    if num >= n:
+        break
+head = n-(num-loc)
+tail = loc+1 - head
+if loc%2 == 0:
+    print("%d/%d" %(head, tail))
+else :
+    print("%d/%d" %(tail, head))
